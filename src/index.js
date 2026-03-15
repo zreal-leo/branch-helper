3220213424cc3add0809288a9883756003411710
罗达成 <luodacheng@comein.cn>
2026-03-15 23:06:42 +0800

    加个successLog

--- src/index.js
+++ src/index.js
@@ -9,61 +9,62 @@ const successLog = msg => console.log(chalk.green(msg));
 const errorLog = msg => console.log(chalk.red(msg));
 
 export function prompt(sourceBranch) {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "branchName",
-                message: "Please enter branch name",
-                validate: value => {
-                    return value.trim() ? true : "分支名不能为空";
-                },
-                filter: val => val.trim()
-            },
-            {
-                type: "input",
-                name: "branchDesc",
-                message: "Please enter branch description",
-                filter: val => val.trim()
-            }
-        ])
-        .then(async answers => {
-            const { branchName, branchDesc } = answers;
-            await createBranch(branchName, sourceBranch);
-            await writeLog({ branchName, branchDesc });
-        })
-        .catch(err => {
-            errorLog(err);
-        });
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "branchName",
+				message: "Please enter branch name",
+				validate: value => {
+					return value.trim() ? true : "分支名不能为空";
+				},
+				filter: val => val.trim()
+			},
+			{
+				type: "input",
+				name: "branchDesc",
+				message: "Please enter branch description",
+				filter: val => val.trim()
+			}
+		])
+		.then(async answers => {
+			const { branchName, branchDesc } = answers;
+			await createBranch(branchName, sourceBranch);
+			await writeLog({ branchName, branchDesc });
+			successLog(`Branch ${branchName} created successfully`);
+		})
+		.catch(err => {
+			errorLog(err);
+		});
 }
 
 async function createBranch(branchName, sourceBranch) {
-    await $`git checkout -b ${branchName} origin/${sourceBranch}`;
+	await $`git checkout -b ${branchName} origin/${sourceBranch}`;
 }
 
 async function writeLog({ branchName, branchDesc }) {
-    const fileName = "branch.log";
-    const filePath = path.join(process.cwd(), fileName);
+	const fileName = "branch.log";
+	const filePath = path.join(process.cwd(), fileName);
 
-    const [err, content = ""] = await to(readJson(filePath));
+	const [err, content = ""] = await to(readJson(filePath));
 
-    const newContent = [
-        {
-            branchName,
-            branchDesc,
-            createTime: new Date().toLocaleString()
-        },
-        ...content
-    ];
-    const [err2] = await to(
-        writeJson(fileName, newContent, {
-            spaces: 4
-        })
-    );
+	const newContent = [
+		{
+			branchName,
+			branchDesc,
+			createTime: new Date().toLocaleString()
+		},
+		...content
+	];
+	const [err2] = await to(
+		writeJson(fileName, newContent, {
+			spaces: 4
+		})
+	);
 
-    if (err2) {
-        errorLog("信息记录失败");
-        process.exit(1);
-    }
-    successLog(`信息记录成功: ${fileName}`);
+	if (err2) {
+		errorLog("信息记录失败");
+		process.exit(1);
+	}
+	successLog(`信息记录成功: ${fileName}`);
 }
